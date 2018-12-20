72c22d8f2c143613c20d344b5bae025736c3c0f8
debugtalk <mail@debugtalk.com>
2018-12-20 12:57:12 +0800

    fix number 1's location

--- programmers-mathematics/chapter5.py
+++ programmers-mathematics/chapter5.py
@@ -10,16 +10,30 @@ import math
 def get_product_factors(n):
     """ get product factors of n
     """
-    if n <= 0:
+    if n <= 0 or not isinstance(n, int):
         return []
     else:
         product_factors = []
         for i in range(1, math.ceil(math.sqrt(n)) + 1):
-            if i > 0 and n % i == 0:
+            if n % i == 0:
                 product_factors.append(i)
                 product_factors.append(int(n / i))
 
         return sorted(list(set(product_factors)))
+
+def print_multiply_combo(result_list):
+    """ add 1 to result_list and print multiply combo
+
+    Args:
+        result_list (list): 乘积列表
+
+    Returns: None
+
+    """
+    for i in range(len(result_list) + 1):
+        new_result_list = copy.copy(result_list)
+        new_result_list.insert(i, 1)
+        print(new_result_list)
 
 def get_multiply_combo(product, result=[]):
     """ 使用函数的递归（嵌套）调用，找出所有可能的乘积组合
@@ -28,40 +42,53 @@ def get_multiply_combo(product, result=[]):
         product: 乘积结果
         result: 保存当前的解
 
-    Returns: void
+    Returns: None
 
     """
     product_factors = get_product_factors(product)
     for i in product_factors:
+        if i == 1:
+            continue
+
         new_result = copy.copy(result)
         new_result.append(i)
+
+        if i == product:
+            print_multiply_combo(new_result)
+
         divisor = product // i
-        if i == 1 or i == product:
-            new_result.append(divisor)
-            print(new_result)
-        else:
+        if divisor != 1:
             get_multiply_combo(divisor, new_result)
 
 
 if __name__ == "__main__":
-    product_factors = get_product_factors(4)
-    print(product_factors)
-    # [1, 2, 4]
+    print(get_product_factors(1))
+    # [1]
+    print(get_product_factors(2))
+    # [1, 2]
+    print(get_product_factors(8))
+    # [1, 2, 4, 8]
 
     get_multiply_combo(6)
-    # [1, 6]
+    # [1, 2, 3]
     # [2, 1, 3]
     # [2, 3, 1]
+    # [1, 3, 2]
     # [3, 1, 2]
     # [3, 2, 1]
+    # [1, 6]
     # [6, 1]
 
     get_multiply_combo(8)
-    # [1, 8]
-    # [2, 1, 4]
+    # [1, 2, 2, 2]
+    # [2, 1, 2, 2]
     # [2, 2, 1, 2]
     # [2, 2, 2, 1]
+    # [1, 2, 4]
+    # [2, 1, 4]
     # [2, 4, 1]
+    # [1, 4, 2]
     # [4, 1, 2]
     # [4, 2, 1]
+    # [1, 8]
     # [8, 1]
